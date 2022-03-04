249d819cc10454bbb7e15f3c2bb522aae3a29c3f
Alicja Latała <alicja.latala12@gmail.com>
2022-03-04 12:39:48 +0100

    Create basec app views

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import AboutViewVue from '@/views/AboutView.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomeView from '../views/HomeView.vue';
@@ -13,10 +14,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: AboutViewVue,
   },
 ];
 
