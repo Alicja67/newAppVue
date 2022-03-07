a47b90c0de129c35732d5ace8605a6daba453f7a
Alicja Latała <alicja.latala12@gmail.com>
2022-03-07 13:25:09 +0100

    Add SearchInput component

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,16 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Hubballi&family=Montserrat:ital,wght@0,300;0,600;1,300&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Hubballi', cursive;
+  font-family: 'Montserrat', sans-serif;
+  padding: 0;
+  margin: 0;
+}
 </style>
