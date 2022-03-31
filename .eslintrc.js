557d5e611c723b40b1bf7d8d577623363bc4ed83
Alicja67 <78256024+Alicja67@users.noreply.github.com>
2022-03-31 23:19:44 +0200

    Add new db.js and data.routes.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "vue/no-v-model-argument": "off",
+    'vue/no-v-model-argument': 'off',
+    'linebreak-style': 0,
   },
   overrides: [
     {
--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db');
+
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json(db.data);
+});
 
-exports.module(router);
+router.post('/', (req, res) => {
+  const title = req.body;
+  const description = req.body;
+  const newData = { nasa_id: uuidv4(), title, description };
+  db.data.push(newData);
+  res.send({
+    message: 'Ok',
+  });
+  console.log(req.body);
+});
+
+router.put('/', (req, res) => {
+
+});
+
+module.exports = router;
